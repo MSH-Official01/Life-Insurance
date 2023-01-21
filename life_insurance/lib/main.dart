f65fd31aaca79503722fb7e3623b08d4bde3b5bd
MSH-Official01 <mdsahadatthossaincse@gmail.com>
2023-01-21 15:26:24 +0600

    Change Title

--- life_insurance/lib/main.dart
+++ life_insurance/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Metropolitan',
+      title: 'Life Insurance',
       debugShowCheckedModeBanner: false,
       home: LogInPage(),
     );
